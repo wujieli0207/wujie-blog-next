c33088788d3c222966cba86cecd7f754376e249f
wujieli0207 <wujieli0207@outlook.com>
2024-06-03 21:57:21 +0800

    feat: 更新 footer 链接

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,18 +3,21 @@ import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
+  const productLink = [
+    {
+      name: 'Hand Drawn AI',
+      href: 'https://handdrawn.ai/',
+    },
+  ]
+
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} /> */}
-          {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
-          {/* <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} /> */}
-          {/* <SocialIcon kind="threads" href={siteMetadata.threads} size={6} /> */}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
@@ -25,7 +28,14 @@ export default function Footer() {
             {siteMetadata.title}
           </Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">岁月神偷，不忘初心</div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          More Product:{' '}
+          {productLink.map((itme) => (
+            <Link key={itme.name} href={itme.href} target="_blank">
+              {itme.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </footer>
   )
